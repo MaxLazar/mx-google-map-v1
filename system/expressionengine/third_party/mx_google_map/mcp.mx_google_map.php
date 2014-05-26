951ed8da41e7d16034997d650265f35227ecaa0b
Max MX <max@eec.ms>
2014-05-26 18:52:09 +0400

    no message

--- system/expressionengine/third_party/mx_google_map/mcp.mx_google_map.php
+++ system/expressionengine/third_party/mx_google_map/mcp.mx_google_map.php
@@ -195,8 +195,6 @@ class Mx_google_map_mcp
 			ee()->view->$key = $val;
 		}
 
-
-		ee()->cp->set_variable('cp_page_title', lang($lang_key));
 		ee()->cp->set_breadcrumb($this->base, lang('mx_google_map_module_name'));
 		
 
